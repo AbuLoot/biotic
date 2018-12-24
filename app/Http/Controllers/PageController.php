b22cb35a22353d69dde31c5c570def048f8b731e
Adl <issayev.adilet@gmail.com>
2018-12-24 22:49:25 +0600

    first commit

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,221 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use DB;
+use URL;
+
+use App\Page;
+use App\News;
+use App\Mode;
+use App\Slide;
+use App\Product;
+use App\Comment;
+use App\Company;
+use App\Category;
+
+class PageController extends Controller
+{
+    public function index()
+    {
+        dd(11);
+        $news = News::where('status', 1)->orderBy('created_at', 'desc')->take(6)->get();
+        $modes = Mode::whereIn('slug', ['new', 'top', 'budgetary'])->get();
+        $slide_mode = Mode::where('slug', 'slide')->first();
+        $slide_items = Slide::where('status', 1)->take(10)->get();
+        $categories_part = Category::whereIn('slug', ['gadjets', 'life-style'])->orderBy('sort_id')->get();
+
+        $ids = collect();
+
+        foreach ($categories_part as $key => $category_item)
+        {
+            if ($category_item->children && count($category_item->children) > 0) {
+                $ids[$key] = $category_item->children->pluck('id');
+            }
+        }
+
+        $products_part = Product::where('status', 1)->whereIn('category_id', $ids[0])->orderBy('sort_id')->take(16)->get();
+        $products_part2 = Product::where('status', 1)->whereIn('category_id', $ids[1])->orderBy('sort_id')->take(16)->get();
+        $group_products = [0 => $products_part, 1 => $products_part2];
+
+        return view('pages.index', compact('news', 'modes', 'slide_mode', 'slide_items', 'categories_part', 'group_products'));
+    }
+
+    public function page($slug)
+    {
+        $page = Page::where('slug', $slug)->firstOrFail();
+
+        return view('pages.page')->with('page', $page);
+    }
+
+    public function allCategoryProducts(Request $request, $category_slug)
+    {
+        $category = Category::where('slug', $category_slug)->first();
+
+        $categories = $category->descendants()->pluck('id');
+
+        // Include the id of category itself
+        $categories[] = $category->getKey();
+
+        // Action operations
+        $actions = ['default' => 'id', 'low' => 'price', 'expensive' => 'price DESC', 'popular' => 'views DESC'];
+        $sort = ($request->session()->has('action')) ? $actions[session('action')] : 'id';
+
+        if ($request->ajax() AND isset($request->action)) {
+            $request->session()->put('action', $request->action);
+        }
+
+        // Option operations
+        if ($request->ajax() AND isset($request->options_id)) {
+            $request->session()->put('options', $request->options_id);
+            $request->session()->put('category_id', $category->id);
+        }
+
+        if ($request->ajax() AND empty($request->action) AND empty($request->options_id) OR session('category_id') != $category->id) {
+            $request->session()->forget('options');
+        }
+
+        if ($request->session()->has('options')) {
+    
+            $options_id = session('options');
+            $products = Product::where('status', '<>', 0)->whereIn('category_id', $categories)->orderByRaw($sort)
+                ->whereHas('options', function ($query) use ($options_id) {
+                    $query->whereIn('option_id', $options_id);
+                })->paginate(12);
+        }
+        else {
+            $products = Product::where('status', '<>', 0)->whereIn('category_id', $categories)->orderByRaw($sort)
+                ->paginate(12);
+        }
+
+        if ($request->ajax()) {
+            return response()->json(view('pages.products-render', ['products' => $products])->render());
+        }
+
+        $options = DB::table('products')
+            ->join('product_option', 'products.id', '=', 'product_option.product_id')
+            ->join('options', 'options.id', '=', 'product_option.option_id')
+            ->select('options.id', 'options.slug', 'options.title', 'options.data')
+            ->whereIn('category_id', $categories)
+            // ->where('products.status', '<>', 0)
+            ->distinct()
+            ->get();
+
+        $grouped = $options->groupBy('data');
+
+        return view('pages.products')->with(['category' => $category, 'products' => $products, 'grouped' => $grouped]);
+    }
+
+    public function categoryProducts(Request $request, $category_slug)
+    {
+        $category = Category::where('slug', $category_slug)->first();
+
+        // Action operations
+        $actions = ['default' => 'id', 'low' => 'price', 'expensive' => 'price DESC', 'popular' => 'views DESC'];
+        $sort = ($request->session()->has('action')) ? $actions[session('action')] : 'id';
+
+        if ($request->ajax() AND isset($request->action)) {
+            $request->session()->put('action', $request->action);
+        }
+
+        // Option operations
+        if ($request->ajax() AND isset($request->options_id)) {
+            $request->session()->put('options', $request->options_id);
+            $request->session()->put('category_id', $category->id);
+        }
+
+        if ($request->ajax() AND empty($request->action) AND empty($request->options_id) OR session('category_id') != $category->id) {
+            $request->session()->forget('options');
+        }
+
+        if ($request->session()->has('options')) {
+
+            $options_id = session('options');
+
+            $products = Product::where('status', '<>', 0)->where('category_id', $category->id)->orderByRaw($sort)
+                ->whereHas('options', function ($query) use ($options_id) {
+                    $query->whereIn('option_id', $options_id);
+                })->paginate(12);
+        }
+        else {
+            $products = Product::where('status', '<>', 0)->where('category_id', $category->id)->orderByRaw($sort)
+                ->paginate(12);
+        }
+
+        if ($request->ajax()) {
+            return response()->json(view('pages.products-render', ['products' => $products])->render());
+        }
+
+        $options = DB::table('products')
+            ->join('product_option', 'products.id', '=', 'product_option.product_id')
+            ->join('options', 'options.id', '=', 'product_option.option_id')
+            ->select('options.id', 'options.slug', 'options.title', 'options.data')
+            ->where('category_id', $category->id)
+            // ->where('products.status', '<>', 0)
+            ->distinct()
+            ->get();
+
+        $grouped = $options->groupBy('data');
+
+        return view('pages.products')->with(['category' => $category, 'products' => $products, 'grouped' => $grouped]);
+    }
+
+    public function brandProducts($company_slug)
+    {
+        $page = Page::where('slug', 'catalog')->firstOrFail();
+        $company = Company::where('slug', $company_slug)->first();
+
+        return view('pages.products')->with(['page' => $page, 'products_title' => $page->title, 'products' => $company->products]);
+    }
+
+    public function product($product_id, $product_slug)
+    {
+        $product = Product::where('id', $product_id)->firstOrFail();
+        $category = Category::where('id', $product->category_id)->firstOrFail();
+        $products = Product::search($product->title)->where('status', 1)->take(4)->get();
+
+        $product->views = $product->views + 1;
+        $product->save();
+
+        return view('pages.product')->with(['product' => $product, 'recent_products' => $products]);
+    }
+
+    public function saveComment(Request $request)
+    {
+        $this->validate($request, [
+            'stars' => 'required|integer|between:1,5',
+            'comment' => 'required|min:5|max:500',
+        ]);
+
+
+        $url = explode('/', URL::previous());
+        $uri = explode('-', end($url));
+
+        if ($request->id == $uri[0]) {
+            $comment = new Comment;
+            $comment->parent_id = $request->id;
+            $comment->parent_type = 'App\Product';
+            $comment->name = \Auth::user()->name;
+            $comment->email = \Auth::user()->email;
+            $comment->comment = $request->comment;
+            $comment->stars = (int) $request->stars;
+            $comment->save();
+        }
+
+        if ($comment) {
+            return redirect()->back()->with('status', 'Отзыв добавлен!');
+        }
+        else {
+            return redirect()->back()->with('status', 'Ошибка!');
+        }
+    }
+
+    public function contacts()
+    {
+        $page = Page::where('slug', 'kontakty')->firstOrFail();
+
+        return view('pages.contacts')->with('page', $page);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,93 @@
+<?php
+
+Auth::routes();
+
+// Authentication routes...
+Route::get('login-or-reg', 'Auth\AuthCustomController@getLogin');
+// Route::post('login', 'Auth\AuthCustomController@postLogin');
+Route::get('logout', 'Auth\AuthCustomController@getLogout');
+
+// Registration routes...
+// Route::get('register', 'Auth\AuthController@getRegister');
+Route::post('register', 'Auth\AuthCustomController@postRegister');
+// Route::get('confirm/{token}', 'Auth\AuthCustomController@confirm');
+
+// Profile
+Route::group(['middleware' => 'auth', 'role:user'], function() {
+
+    Route::get('my-profile', 'ProfileController@profile');
+    Route::post('my-profile', 'ProfileController@updateProfile');
+    Route::get('my-profile/edit', 'ProfileController@editProfile');
+    Route::get('my-orders', 'ProfileController@myOrders');
+});
+
+// Joystick Administration
+Route::group(['prefix' => 'admin', 'middleware' => ['auth', 'role:admin']], function () {
+
+    Route::get('/', 'Joystick\AdminController@index');
+
+    Route::resource('categories', 'Joystick\CategoryController');
+    Route::resource('countries', 'Joystick\CountryController');
+    Route::resource('companies', 'Joystick\CompanyController');
+    Route::resource('cities', 'Joystick\CityController');
+    Route::resource('news', 'Joystick\NewsController');
+    Route::resource('languages', 'Joystick\LanguageController');
+    Route::resource('modes', 'Joystick\ModeController');
+    Route::resource('options', 'Joystick\OptionController');
+    Route::resource('orders', 'Joystick\OrderController');
+    Route::resource('pages', 'Joystick\PageController');
+    Route::resource('products', 'Joystick\ProductController');
+    Route::resource('slide', 'Joystick\SlideController');
+    Route::get('products/{id}/comments', 'Joystick\ProductController@comments');
+    Route::get('products/{id}/destroy-comment', 'Joystick\ProductController@destroyComment');
+    Route::get('products-search', 'Joystick\ProductController@search');
+    Route::get('products-category/{id}', 'Joystick\ProductController@categoryProducts');
+    Route::get('products-actions', 'Joystick\ProductController@actionProducts');
+    Route::get('products-price/edit', 'Joystick\ProductController@priceForm');
+    Route::post('products-price/update', 'Joystick\ProductController@priceUpdate');
+
+    Route::resource('roles', 'Joystick\RoleController');
+    Route::resource('users', 'Joystick\UserController');
+    Route::resource('permissions', 'Joystick\PermissionController');
+
+    Route::get('apps', 'Joystick\AppController@index');
+    Route::get('apps/{id}', 'Joystick\AppController@show');
+    Route::delete('apps/{id}', 'Joystick\AppController@destroy');
+});
+
+
+// Input
+Route::get('search', 'InputController@search');
+Route::get('search-ajax', 'InputController@searchAjax');
+Route::post('filter-products', 'InputController@filterProducts');
+Route::post('send-app', 'InputController@sendApp');
+
+
+// Basket Actions
+Route::get('add-to-basket/{id}', 'BasketController@addToBasket');
+Route::get('remove-from-basket/{id}', 'BasketController@removeFromBasket');
+Route::get('clear-basket', 'BasketController@clearBasket');
+Route::get('basket', 'BasketController@basket');
+Route::get('basket/{id}', 'BasketController@destroy');
+Route::post('store-order', 'BasketController@storeOrder');
+
+
+// Favorite Actions
+Route::get('favorites', 'FavoriteController@getFavorites');
+Route::get('toggle-favorite/{id}', 'FavoriteController@toggleFavorite');
+
+
+// Pages
+Route::get('/', 'PageController@index');
+Route::get('catalog', 'PageController@catalog');
+Route::get('catalog/all/{category}', 'PageController@allCategoryProducts');
+Route::get('catalog/{category}', 'PageController@categoryProducts');
+Route::get('goods/{id}-{product}', 'PageController@product');
+Route::post('comment-product', 'PageController@saveComment');
+Route::get('catalog/brand/{company}', 'PageController@brandProducts');
+Route::get('news', 'NewsController@news');
+Route::get('news-category/{page}', 'NewsController@newsCategory');
+Route::get('news/{page}', 'NewsController@newsSingle');
+Route::post('comment-news', 'NewsController@saveComment');
+Route::get('kontakty', 'PageController@contacts');
+Route::get('{page}', 'PageController@page');
