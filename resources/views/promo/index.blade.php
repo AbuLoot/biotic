4706132b351adf5793e88974869ecb1b10d292e8
Adl <issayev.adilet@gmail.com>
2019-05-27 18:39:23 +0600

    changed link

--- resources/views/promo/index.blade.php
+++ resources/views/promo/index.blade.php
@@ -17,7 +17,7 @@
         <div class="col-md-6 presentation-epimedium pt-7">
           <h2 class="ts-1">ЭПИМЕДИУМНАЯ ПАСТА</h2>
           <p class="ts-1">Уникальный рецепт половой силы и здоровья для мужчин и женщин из Турции</p>
-          <a class="button style-outline button-xs button-secondary fz-20" href="/promo/epimediumnaya-pasta">Подробнее</a>
+          <a class="button style-outline button-xs button-secondary fz-20" href="/epimediumnaya-pasta">Подробнее</a>
           <!-- <img src="/img/epimedium.jpg" class="img-responsive pull-right"> -->
         </div>
       <!-- </a> -->
@@ -25,7 +25,7 @@
         <div class="col-md-6 presentation-collagen pt-7">
           <h2>100% ГОВЯЖИЙ ПРОТЕИН</h2>
           <p>Протеин полученный из натурального говяжьего белка, для набора мышечной массы, для укрепления суставов и связок</p>
-          <a class="button style-outline button-xs button-primary fz-20" href="/promo/protein">Подробнее</a>
+          <a class="button style-outline button-xs button-primary fz-20" href="/protein">Подробнее</a>
           <!-- <img src="/img/collagen.jpg" class="img-responsive"> -->
         </div>
       <!-- </a> -->
